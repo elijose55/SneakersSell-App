48421e1cecfff2bf2ed8d80b51b96e96e8b98270
Nicolas <nicolasbs@al.insper.edu.br>
2018-05-15 18:08:37 -0300

    sell3activity

--- Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell3Activity.java
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell3Activity.java
@@ -1,13 +1,44 @@
 package com.example.gabrielmoura.ace1;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Sell3Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell3);
+
+
+        Button Prosseguir = (Button) findViewById(R.id.NextButton);
+
+        Prosseguir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSell4ObservationActivity();
+            }
+        });
+    }
+
+    public void openSell4ObservationActivity() {
+        Intent intent4 = new Intent(this, Sell4ObservationActivity);
+        startActivity(intent4);
+        finishActivity();
+        return; 
+    };
+
+
+    @Override
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, Sell2RulesAcivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
     }
 }
