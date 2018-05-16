17f6a997bf818daa589f1abbdfb95066d0c8c787
Gabrielmou <gabrielmoura98@gmail.com>
2018-05-15 21:54:02 -0300

    Atualizando

--- Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell2RulesActivity.java
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell2RulesActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 public class Sell2RulesActivity extends AppCompatActivity {
 
-    private void openSell3() {
+    private CheckBox accept;
+
+    private void openSell3Activity() {
         Intent intent = new Intent(this, Sell3Activity.class);
         startActivity(intent);
         finish();
@@ -18,18 +23,26 @@ public class Sell2RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell2rules);
-        Button accept= (Button) findViewById(R.id.AcceptTermsButton);
-
+       // onCheckboxClicked();
+    //}
+    //public void onCheckboxClicked() {
+        accept = (CheckBox) findViewById(R.id.AcceptTermsButton);
         accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSell3();
+                //is chkIos checked?
+                if (((CheckBox) v).isChecked()) {
+                    Toast.makeText(Sell2RulesActivity.this,
+                            "Bro, try Android :)", Toast.LENGTH_LONG).show();
+                    //openSell3Activity();
+                }
             }
         });
     }
+
     @Override
     public void onBackPressed(){ //Botão BACK padrão do android
-        startActivity(new Intent(this, MainActivity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        startActivity(new Intent(this, Sell1Activity.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
         finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
         return;
     }
