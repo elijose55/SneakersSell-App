a37443f5fc02b7a66f85b24ce47250bd3bbe7b5f
elijose55 <elijose55@hotmail.com>
2018-05-16 02:54:14 -0300

    tela do produto feita com imagem ja colocada, precisa testar e realizar alguns ajustes

---
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/sneaker_detail.java
@@ -0,0 +1,33 @@
+package com.example.gabrielmoura.ace1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+
+public class sneaker_detail extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+}
