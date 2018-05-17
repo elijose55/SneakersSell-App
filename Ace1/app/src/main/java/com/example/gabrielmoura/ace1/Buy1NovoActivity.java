8e1633d70fda2142abca76809775a3c6761b5f7c
duda <mariaeduardabicalho@gmail.com>
2018-05-17 15:02:27 -0300

    novos comprar

---
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Buy1NovoActivity.java
@@ -0,0 +1,18 @@
+package com.example.gabrielmoura.ace1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Buy1NovoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.novobuy1);
+    }
+    public void onBackPressed(){ //Botão BACK padrão do android
+        startActivity(new Intent(this, sneaker_detail.class)); //O efeito ao ser pressionado do botão (no caso abre a activity)
+        finishAffinity(); //Método para matar a activity e não deixa-lá indexada na pilhagem
+        return;
+    }
+}
