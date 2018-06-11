844e5dc5ccd1eaf8baaadf0e84bf93a18673f388
Nicolas <nicolasbs@al.insper.edu.br>
2018-06-11 13:04:30 -0300

    registro

--- Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell1Activity.java
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell1Activity.java
@@ -1,14 +1,34 @@
 package com.example.gabrielmoura.ace1;
 
+import android.app.ProgressDialog;
 import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sell1Activity extends AppCompatActivity {
+
+    private static final String TAG = "s";
+    private Button next;
+    private EditText nome;
+    private EditText email;
+    private EditText senha;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     private void openSell2RulesActivity() {
         Intent intent = new Intent(this, Sell2RulesActivity.class);
@@ -24,10 +44,16 @@ public class Sell1Activity extends AppCompatActivity {
         ImageButton next = (ImageButton) findViewById(R.id.nextbuttonsell1);
         ImageButton back = (ImageButton) findViewById(R.id.backbuttonsell1);
 
+        firebaseAuth = firebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        email = (EditText) findViewById(R.id.emailID);
+        senha = (EditText) findViewById(R.id.senhaID);
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openSell2RulesActivity();
+                registerUser();
+
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +75,38 @@ public class Sell1Activity extends AppCompatActivity {
         Intent intent = new Intent(this, TabPages.class);
         startActivity(intent);
         finish();
+    }
+
+    private void registerUser() {
+        String idemail = email.getText().toString().trim();
+        String password = senha.getText().toString().trim();
+
+        if(TextUtils.isEmpty(idemail)){
+            //email is empty
+            Toast.makeText(this, "Please enter  email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            //senha is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+        firebaseAuth.createUserWithEmailAndPassword(idemail,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(Sell1Activity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            openSell2RulesActivity();
+                        }else{
+                            Toast.makeText(Sell1Activity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG,"create Account: Fail!", task.getException());
+                        }
+                    }
+                });
+
     }
 }
