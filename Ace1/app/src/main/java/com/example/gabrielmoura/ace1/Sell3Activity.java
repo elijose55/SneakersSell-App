40e3c0e7043b420c5023c6a8934245489f590029
Gabrielmou <gabrielmoura98@gmail.com>
2018-06-11 01:47:37 -0300

    Spinners

--- Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell3Activity.java
+++ Ace1/app/src/main/java/com/example/gabrielmoura/ace1/Sell3Activity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class Sell3Activity extends AppCompatActivity {
 
@@ -22,6 +23,18 @@ public class Sell3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell3);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.tamanhospin);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.tamanho, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.estadospin);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.estado, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
 
         ImageButton next = (ImageButton) findViewById(R.id.nextbuttonsell3);
         ImageButton back = (ImageButton) findViewById(R.id.backbuttonsell3);
